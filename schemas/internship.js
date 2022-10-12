d446393ace1b140620aafec523e6bf6db81a1657
Gyana Ranjan Panda <grp.gyanaranjan@gmail.com>
2022-10-13 01:30:15 +0530

    portfolio backend ready

---
+++ schemas/internship.js
@@ -0,0 +1,54 @@
+export default {
+    name: 'internship',
+    type: 'document',
+    title: 'Internship',
+    fields: [
+        {
+            name: 'title',
+            type: 'string',
+            title: 'Title'
+
+        },
+        {
+            name:'from',
+            type: 'datetime',
+            title: 'From'
+        },
+        {
+            name:'to',
+            type: 'datetime',
+            title: 'To'
+        },
+        {
+            name: 'image',
+            type: 'image',
+            title: 'Image'
+        },
+        {
+            name: 'shortDescription',
+            type: 'string',
+            title: 'Short Description'
+        },
+        {
+            name: 'desc',
+            type: 'array',
+            title: 'Description',
+            of: [
+                {
+                    type: 'block'
+                }
+            ]
+        }
+        , {
+            name: 'stacks',
+            type: 'array',
+            title: 'Stacks',
+            of: [
+                {
+                    type: 'string'
+                }
+            ]
+        }
+    ]
+
+}
---
+++ schemas/work.js
@@ -0,0 +1,43 @@
+export default {
+    name: 'work',
+    type: 'document',
+    title: 'Work',
+    fields: [
+        
+        {
+            name: 'title',
+            type: 'string',
+            title: 'Title'
+        },
+        {
+            name: 'shortDescription',
+            type: 'string',
+            title: 'Short Description'
+        },
+        {
+            name: 'desc',
+            type: 'string',
+            title: 'Description'
+        },
+        {
+            name: 'image',
+            type: 'array',
+            title: 'Image',
+            of:[
+              {
+                type: 'image',
+              }
+            ]
+        },
+        {
+            name: 'stacks',
+            type: 'array',
+            title: 'Stacks',
+            of: [
+                {
+                    type: 'string'
+                }
+            ]
+        }
+    ]
+}
